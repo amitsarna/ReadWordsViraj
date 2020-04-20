ec9d0eb967e4d7aea76876e9a1095696fcb1d93b
amitsarna <amit.sarna.584@gmail.com>
2020-04-20 08:55:23 +0100

    Updates

---
+++ precache-manifest.6c305b6bae16a5a6f8b3a0e4ec39d520.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02e68f3b69bc59977c6c5cd4f71318f9",
+    "url": "/ReadWordsViraj/index.html"
+  },
+  {
+    "revision": "dc7ae8d27ec98e4e911b",
+    "url": "/ReadWordsViraj/static/css/main.bffb2367.chunk.css"
+  },
+  {
+    "revision": "3efc7ddb39cc7302054b",
+    "url": "/ReadWordsViraj/static/js/2.bc66cd20.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReadWordsViraj/static/js/2.bc66cd20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc7ae8d27ec98e4e911b",
+    "url": "/ReadWordsViraj/static/js/main.fba55e2b.chunk.js"
+  },
+  {
+    "revision": "021f73dcaecc2d2c32a3",
+    "url": "/ReadWordsViraj/static/js/runtime-main.a636f56a.js"
+  }
+]);
